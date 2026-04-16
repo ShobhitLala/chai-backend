2882b8f192f4a933bba59819ab4e51fd7cddd24e
ShobhitLala <shobhitsrivastava1001@gmail.com>
2026-04-16 22:45:03 +0530

    logout features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,19 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/Apiresponse.js"
+
+const generateAccesstokenansRefreshToken=async(userID)=>{
+  try {
+    const user=await User.findById(userID);
+    const accessToken=user.generateAccessToken()
+    const  refreshToken=user.generateRefreshToken()
+    user.refreshToken=refreshToken;
+     await user.save({validateBeforeSave:false})
+     return {accessToken,refreshToken};
+  } catch (error) {
+    throw new ApiError(500,"something went wrong while creating tokens ")
+  }
+}
 const registerUser=asyncHandler(async(req,res)=>{
     //get user details from frontend
     //validation-not empty
@@ -71,7 +84,73 @@ return res.status(201).json(
 )
 
 })
+const loginUser=asyncHandler(async(req,res)=>{
+    //from reqbody we need to take usernmae/eamil and password;
+    //we will validate usr
+    //we will match our password
+    // if password is valid we send cookies to the user or acesstokenn
+    const {userName,email,password}=req.body
+    if(!userName && !email){
+      throw new ApiError(400,"userName/Email is required");
+    }
+      
+    const user=await User.findOne({
+     $or:[{userName},{email}]
+    })
+    if(!user){
+      throw new ApiError(404,"user not found with such credentials");
+    }
+   const isPasswordcorrect= await user.isPasswordCorrect(password);
+   if(!isPasswordcorrect){
+    throw new ApiError(404,"password is wrong");
+   }
+   const{accessToken,refreshToken}=await generateAccesstokenansRefreshToken(user._id)
+   const loggedinUser=await User.findById(user._id).select("-password-refreshToken");
+  const options={
+    httpOnly:true,
+    secure:true
+  }
+  return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(
+          new ApiResponse(
+           200,
+           {
+           user: loggedinUser,accessToken,refreshToken
+           },
+           "User logged in sucessfully"
+          )
+        )
+
+})
+const logoutUser=asyncHandler(async(req,res)=>{
+      await User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $set:{
+            refreshToken:undefined
+          }
+        },
+        {
+          new:true
+        }
+      )
+      const options={
+    httpOnly:true,
+    secure:true
+  }
+  return res
+  .status(200)
+  .clearCookie("accessToken",options)
+  .clearCookie("refreshToken",options)
+  .json(
+    new ApiResponse(200,{},"User loggedout")
+  )
+
+})
+ 
 
 
-
-export{registerUser}
+export{registerUser,loginUser,logoutUser}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs"
-console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API Key:", process.env.CLOUDINARY_CLOUD_API_KEY);
-console.log("API Secret:", process.env.CLOUDINARY_CLOUD_API_SECRET);
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key:    process.env.CLOUDINARY_CLOUD_API_KEY, 
